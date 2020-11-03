5705a9bfc1d746d6b7606e3844efeccc35d4733f
Bonan Zhu <zhubonan@outlook.com>
2020-11-03 12:42:47 +0000

    Bug fix

--- aiida_user_addons/tools/phonopy.py
+++ aiida_user_addons/tools/phonopy.py
@@ -25,10 +25,10 @@ def export_phonon_work(work, dst, include_potcar=False):
         export_vasp_calc(node, fc_folder, decompress=True, include_potcar=include_potcar)
 
     # Write POSCAR file used from creating the displacements
-    if settings in work.inputs:
-        settings = work.inputs.settings.get_dict()
+    if 'relax_structure' in work.outputs:
+        poscar_parser = PoscarParser(data=work.outputs.relax_structure, precision=10)
     else:
-        settings = {}
-    poscar_precision = settings.get('poscar_precision', 10)
-    poscar_parser = PoscarParser(data=work.outputs.relax_structure(), precision=poscar_precision)
+        # In this case the initial structure was used as it is
+        poscar_parser = PoscarParser(data=work.inputs.structure, precision=10)
+
     poscar_parser.write(str(dst / 'POSCAR'))
