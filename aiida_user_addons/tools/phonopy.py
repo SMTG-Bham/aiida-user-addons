131ae3d7244e57aed6c5ac14051ef73bb57d9cf6
Bonan Zhu <zhubonan@outlook.com>
2020-11-03 15:08:33 +0000

    Added function for reobtain phonopy object

--- aiida_user_addons/tools/phonopy.py
+++ aiida_user_addons/tools/phonopy.py
@@ -4,6 +4,7 @@ Tool for working with Phonopy calculations
 
 from pathlib import Path
 from aiida_vasp.parsers.file_parsers.poscar import PoscarParser
+from aiida_phonopy.common.utils import get_phonopy_instance
 
 from .vasp import export_vasp_calc
 
@@ -32,3 +33,29 @@ def export_phonon_work(work, dst, include_potcar=False):
         poscar_parser = PoscarParser(data=work.inputs.structure, precision=10)
 
     poscar_parser.write(str(dst / 'POSCAR'))
+
+
+def get_phonon_obj(work, nac=None):
+    """
+    Reconstruct a phonopy object from finished/unfinished workchain.
+
+    Useful when recomputing certain properties are needed.
+    """
+    force_set = work.outputs.force_sets
+    if nac:
+        params = {'nac_params': work.outputs.nac_params if nac == 'auto' else nac}
+    else:
+        params = {}
+    phonon = get_phonopy_instance(work.outputs.relaxed_structure, work.outputs.phonon_setting_info, params)
+
+    # Treat the magmom
+    try:
+        phonon.unitcell.set_magnetic_moments(work.inputs.structure.get_attribute('MAGMOM'))
+    except (KeyError, AttributeError):
+        pass
+
+    displacements = work.outputs.phonon_setting_info['displacement_dataset']
+    phonon.dataset = displacements
+    phonon.set_forces(force_set.get_array('force_sets'))
+    phonon.produce_force_constants()
+    return phonon
