62934be69558fd939482f944b77f5f27a0991201
Bonan Zhu <zhubonan@outlook.com>
2020-12-06 15:50:58 +0000

    Added more comprehensive export for phonopy calculations

--- aiida_user_addons/tools/phonopy.py
+++ aiida_user_addons/tools/phonopy.py
@@ -5,6 +5,7 @@ Tool for working with Phonopy calculations
 from pathlib import Path
 from warnings import warn
 from aiida_vasp.parsers.file_parsers.poscar import PoscarParser
+from aiida.common.exceptions import NotExistent
 from aiida_phonopy.common.utils import get_phonopy_instance
 
 from .vasp import export_vasp_calc
@@ -18,6 +19,7 @@ def export_phonon_work(work, dst, include_potcar=False):
     to re-obtain the forces
     """
     from aiida.orm import Node, QueryBuilder, StructureData, CalcFunctionNode
+    from phonopy.file_IO import write_BORN, write_FORCE_CONSTANTS, write_FORCE_SETS
 
     dst = Path(dst)
     dst.mkdir(exist_ok=True)
@@ -30,6 +32,7 @@ def export_phonon_work(work, dst, include_potcar=False):
     nac_calc = work.get_outgoing(link_label_filter='nac_calc').first().node
     if nac_calc:
         export_vasp_calc(nac_calc, (dst / 'nac_calc'), decompress=True, include_potcar=include_potcar)
+        warn('NAC calculation')
 
     # Write POSCAR file used from creating the displacements
     q = QueryBuilder()
@@ -43,18 +46,37 @@ def export_phonon_work(work, dst, include_potcar=False):
     poscar_parser = PoscarParser(data=disp_structure, precision=10)
     poscar_parser.write(str(dst / 'POSCAR'))
 
+    # Write processed phonon data
+    pobj = get_phonon_obj(work, nac='auto')
 
-def get_phonon_obj(work, nac=None):
+    # phonopy_disp.yaml
+    pobj.save(dst / 'phonopy_disp.yaml')
+    pobj.save(dst / 'phonopy_params.yaml')
+
+    # FORCE_SETS and FORCE_CONSTANTS
+    write_FORCE_SETS(pobj.dataset, dst / 'FORCE_SETS')
+    write_FORCE_CONSTANTS(pobj.get_force_constants(), dst / 'FORCE_CONSTANTS')
+
+    # Export BORN FILE
+    if nac_calc:
+        write_BORN(pobj.primitive, pobj.nac_params['borns'], pobj.nac_params['dielectric'], dst / 'BORN')
+
+
+def get_phonon_obj(work, nac='auto'):
     """
     Reconstruct a phonopy object from finished/unfinished workchain.
 
     Useful when recomputing certain properties are needed.
     """
     force_set = work.outputs.force_sets
-    if nac:
-        params = {'nac_params': work.outputs.nac_params if nac == 'auto' else nac}
+    if nac == 'auto':
+        if 'nac_params' in work.outputs:
+            params = {'nac_params': work.outputs.nac_params}
+    elif nac:
+        params = {'nac_params': nac}
     else:
         params = {}
+
     phonon = get_phonopy_instance(work.outputs.relaxed_structure, work.outputs.phonon_setting_info, params)
 
     # Treat the magmom
@@ -66,5 +88,9 @@ def get_phonon_obj(work, nac=None):
     displacements = work.outputs.phonon_setting_info['displacement_dataset']
     phonon.dataset = displacements
     phonon.set_forces(force_set.get_array('force_sets'))
-    phonon.produce_force_constants()
+    try:
+        phonon.set_force_constants(work.outputs.force_constants.get_array('force_constants'))
+    except (AttributeError, KeyError, NotExistent):
+        phonon.produce_force_constants()
+        warn('Cannot locate force constants - producing force constants from force_sets.')
     return phonon
