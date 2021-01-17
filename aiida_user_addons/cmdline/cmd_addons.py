2a453143425c843bc8e21a18febbc6e6a605838a
Bonan Zhu <zhubonan@outlook.com>
2021-01-17 17:36:21 +0000

    Added remotepull command

--- aiida_user_addons/cmdline/cmd_addons.py
+++ aiida_user_addons/cmdline/cmd_addons.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from aiida_user_addons.tools.scfcheck import database_sweep
 from aiida.cmdline.commands.cmd_data import verdi_data
 from aiida.cmdline.params.arguments import PROCESS, WORKFLOW, CALCULATION
+from aiida.cmdline.utils.echo import echo_success, echo_info, echo_error
 
 
 @verdi_data.group('addons')
@@ -86,3 +87,30 @@ def remotecat(calcjob, fname, save_to):
     if save_to is None:
         os.close(fd)
         os.remove(temppath)
+
+
+@addons.command('remotepull')
+@CALCULATION('calcjob')
+@click.argument('dest')
+@click.option('--max-size', '-m', help='Maximum size of the files to be retrieved - this is passed to rsync')
+def remotepull(calcjob, dest, max_size):
+    """Cat the content of an file lying on the remote folder of the calculation"""
+    import subprocess
+    rfolder = calcjob.outputs.remote_folder
+    cmd_args = ['rsync', '-av']
+
+    if max_size:
+        cmd_args.extend(['--max-size', max_size])
+
+    cmd_args.append('{}:{}/'.format(rfolder.computer.hostname, rfolder.get_remote_path()))
+    if not dest.endswith('/'):
+        dest = dest + '/'
+    cmd_args.append(dest)
+
+    echo_info('Running commands: {}'.format(' '.join(cmd_args)))
+
+    completed = subprocess.run(cmd_args)
+    if completed.returncode != 0:
+        echo_error('Failled to pull data using rsync')
+    else:
+        echo_success('Remote folder pulled to {}'.format(dest))
