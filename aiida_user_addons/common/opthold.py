f5fc92101c75f81de619c99813a026443f34ee2c
Bonan Zhu <zhubonan@outlook.com>
2021-08-01 12:18:46 +0100

    Created more advance OptionContainer class
    The OptionContainer class is a replacement for OptionHolder. It uses
    the descriptor interface to allow dynamic checking/convertion of the
    option field passed. Specific OptionContainer classes can be created
    in a similar but more streamlined way compared to the old OptionHolder.

--- aiida_user_addons/common/opthold.py
+++ aiida_user_addons/common/opthold.py
@@ -1,8 +1,248 @@
 """
 Module containing the OptionHolder class
 """
+from typing import Tuple, List
 from aiida.common.extendeddicts import AttributeDict
 from aiida.common.exceptions import InputValidationError
+from attr import setters
+
+
+class Option:
+    """
+    Base class for descriptors to be used as pre-defined fields for `OptioinContainer`.
+
+    The point of using these seemingly complex descriptors is to allow the target `OptionContainer`
+    class to support pre-defined properties acting as the fields to be set with the following
+    functionalities:
+
+    * Tab completion of the field name
+    * Assignment time checks of the correct object types
+    * Default values at a per `OptionHolder` subclass level
+    * Enforcement of a field being required, e.g. no default value is avaliable.
+    * Automatic type convertion where necessary
+
+    """
+
+    def __init__(self, docstring, default_value=None, required=False):
+        """Initialise an option and passing the docstring"""
+        self.__doc__ = docstring
+        self.required = required
+        self.default_value = default_value
+        self.name = None
+
+    def __set_name__(self, owner, name):
+        """Methods for automatically setting the `name` attribute - works for python 3.6+ only"""
+        self.name = name
+
+    def __get__(self, obj, owner=None):
+        """Get the stored value"""
+        _ = owner
+        if self.required and self.name not in obj._opt_data:
+            raise ValueError(f'Field {self.name} has not been set yet!')
+
+        return obj._opt_data.get(self.name, self.default_value)
+
+    def __set__(self, obj, value):
+        obj._opt_data[self.name] = value
+
+    def __delete__(self, obj):
+        """Delete the option from the holder dictionary"""
+        if self.name in obj._opt_data:
+            del obj._opt_data[self.name]
+
+
+class TypedOption(Option):
+    """Class for an option that enforces a specific type"""
+
+    target_type = bool
+
+    def __init__(self, docstring, default_value=None, required=False, enforce_type=False):
+        """Instantiate an TypedOption field"""
+        super().__init__(docstring, default_value, required)
+        self.enforce_type = enforce_type
+
+    def __set__(self, obj, value):
+        """Setter for setting the option"""
+        if self.enforce_type:
+            if isinstance(value, self.target_type):
+                obj._opt_data[self.name] = value
+            else:
+                raise ValueError(f'{value} is not a {self.target_type} type!')
+        else:
+            obj._opt_data[self.name] = self.target_type(value)
+
+    def __get__(self, obj, owner=None):
+        return self.target_type(super().__get__(obj, owner))
+
+
+class BoolOption(TypedOption):
+    """Class for an option that accepts bool values"""
+    target_type = bool
+
+
+class FloatOption(TypedOption):
+    """Class for an option that accepts float values"""
+    target_type = float
+
+
+class IntOption(TypedOption):
+    """Class for an option that accepts integer values"""
+    target_type = int
+
+
+class OptionContainer:
+    """
+    Base class for a container of options
+    """
+
+    def __init__(self, **kwargs):
+        """
+        A holder of options
+
+        Arguments:
+            kwargs: unpack keyword arguments and set them as the attributes
+        """
+        self._opt_data = dict()
+
+        # the key word arguments are interpreted as the options to be set, an we check
+        # If they are valid in the first place
+        self.valid_options, self.required_options = self._get_valid_and_required_options()
+        for key, value in kwargs.items():
+            if key in self.valid_options:
+                setattr(self, key, value)
+            else:
+                raise ValueError(f'{key} is not a valid option for a {type(self)} instance!')
+
+    def _get_valid_and_required_options(self) -> Tuple[list, list]:
+        """
+        Return a list of valid option names
+
+        This method extracts a tuple of list of the valid option names and those that are
+        marked as `required`.
+        """
+        options = []
+        required = []
+        class_dict = vars(type(self))
+        for name in dir(self):
+            if name.startswith('__'):
+                pass
+            # Check if the name exists in the class's __dict__
+            if name not in class_dict:
+                continue
+            optobj = class_dict.get(name)
+
+            # Check if the name is an Option
+            if isinstance(optobj, Option):
+                options.append(name)
+                if optobj.required:
+                    required.append(name)
+        return options, required
+
+    @property
+    def _invalid_attributes(self) -> List[str]:
+        """Any attribute store inside __dict__ is an invalid option"""
+        known = ['_opt_data', 'valid_options', 'required_options']
+        invalid = []
+        for key in self.__dict__.keys():
+            if key not in known:
+                invalid.append(key)
+        return invalid
+
+    def to_dict(self) -> AttributeDict:
+        """Return a python dict representation - including all fields"""
+        # Check for any additional attributes
+        invalid_attrs = self._invalid_attributes
+        if invalid_attrs:
+            raise ValueError(f'The following attributes are not valid options: {invalid_attrs}')
+
+        return AttributeDict({key: getattr(self, key) for key in self.valid_options})
+
+    def to_aiida_dict(self):
+        """Return an `aiida.orm.Dict` presentation """
+        from aiida.orm import Dict
+        python_dict = self.to_dict()
+        return Dict(dict=python_dict)
+
+    def __setitem__(self, key, value) -> None:
+        """Set items - we call the setattr method"""
+        if key not in self.valid_options:
+            raise KeyError(f'{key} is not an valid option for a {type(self)} instance.')
+        setattr(self, key, value)
+
+    def to_string(self) -> str:
+        """In string format"""
+        return self.to_dict().__repr__().replace('AttributeDict', '')
+
+    def __getitem__(self, key):
+        """Set items - we just the getattr method"""
+        return getattr(self, key)
+
+    def __repr__(self):
+        string = self.to_string()
+        string = string.replace('\n', ' ')
+        string = string.replace('#', '')
+        string = string.strip()
+        if len(string) > 60:
+            string = string[:60] + '...'
+        return '{}<{}>'.format(type(self).__name__, string)
+
+    @classmethod
+    def validate_dict(cls, input_dict, port=None) -> None:
+        """
+        Vaildate a dictionary/Dict node, this can be used as the validator for
+        the Port accepting the inputs
+        """
+        obj = cls(**input_dict)
+        all_options = list(obj.valid_options)  # A copy for all options
+
+        # Are we receiving a Dict object?
+        if not isinstance(input_dict, dict):
+            input_dict = input_dict.get_dict()
+
+        for key in input_dict.keys():
+            if key not in all_options:
+                raise InputValidationError(f"Key '{key}' is not a valid option")
+            all_options.remove(key)
+
+        # Check for any missing required fields
+        missing = [key for key in all_options if key in obj.required_options]
+        if missing:
+            raise InputValidationError(f'There are missing options: {missing}')
+
+        # Check for any problems with obj.to_dict()
+        try:
+            obj.to_dict()
+        except ValueError as error:
+            raise InputValidationError(f'Error during validation: {error.args}')
+
+    @classmethod
+    def serialise(cls, value):
+        """
+        Serialise a dictionary into Dict
+
+        This method can be passed as a `serializer` key word parameter of for the `spec.input` call.
+        """
+        obj = cls(**value)
+        return obj.to_aiida_dict()
+
+    @classmethod
+    def setup_spec(cls, spec, port_name, **kwargs) -> None:
+        """Setup the spec for this input"""
+        # Check if we have 'required' fields
+        obj = cls()
+        # The constructor is different with/without any required_options
+        # If there are any required options, it does not make any sense to have a default for the port.
+        if obj.required_options:
+            spec.input(port_name, validator=cls.validate_dict, serializer=cls.serialise, **kwargs)
+        else:
+            spec.input(port_name, validator=cls.validate_dict, default=lambda: cls().to_aiida_dict(), serializer=cls.serialise, **kwargs)
+
+
+########
+#
+#  Old option holder code
+#
+# ######
 
 
 def typed_field(name, types, doc, default):
--- aiida_user_addons/common/test_opthold.py
+++ aiida_user_addons/common/test_opthold.py
@@ -3,7 +3,111 @@ Test the utilties
 """
 import pytest
 from aiida.common.exceptions import InputValidationError
-from .opthold import OptionHolder, typed_field, required_field
+from .opthold import (IntOption, OptionContainer, OptionHolder, typed_field, required_field, FloatOption, BoolOption, TypedOption, Option,
+                      OptionContainer)
+
+##### Tests for the new option holder interface using descriptors
+
+
+class DummyOptionClass:
+
+    a = Option('test-option-a')
+    b = Option('test-option-b', 0)
+    c = Option('test-option-c', 0, True)
+
+    def __init__(self):
+
+        self._opt_data = {}
+
+
+class DummyOptionClassWithType(DummyOptionClass):
+
+    a = BoolOption('test option')
+    b = BoolOption('test option', default_value=True, enforce_type=True)
+    c = BoolOption('test option', required=True)
+    d = IntOption('test option', default_value=3, enforce_type=True)
+    e = FloatOption('test option', default_value=2, enforce_type=False)
+
+
+def test_dummy_option_class():
+    """Test for the dummy option class"""
+
+    obj = DummyOptionClass()
+
+    # Check for the accessor methods
+    assert obj.a == None
+    assert obj.b == 0
+    with pytest.raises(ValueError):
+        _ = obj.c
+
+    obj.b = 10
+    assert obj.b == 10
+
+
+def test_dummy_option_class_with_type():
+    """Test for the dummy option class with typed options"""
+
+    obj = DummyOptionClassWithType()
+
+    assert obj.a == False
+    assert obj.b == True
+    with pytest.raises(ValueError):
+        obj.b = 2
+
+    with pytest.raises(ValueError):
+        y = obj.c
+
+    assert obj.d == 3
+
+    # Raising type inconsistency errors
+    with pytest.raises(ValueError):
+        obj.d = 3.0
+
+    with pytest.raises(ValueError):
+        obj.e = 'abc'
+
+    obj.e = '10.2'
+    assert obj.e == 10.2
+
+
+class DummyContainer(OptionContainer):
+
+    a = FloatOption('test', 2.0)
+    b = FloatOption('test', 2.0, required=True)
+
+
+def test_option_container():
+    """Test the option container"""
+
+    cont = DummyContainer()
+    assert cont.valid_options == ['a', 'b']
+    assert cont.required_options == ['b']
+    assert cont.a == 2.0
+
+    # This should raise an error as 'b' has not been set yet
+    with pytest.raises(ValueError, match='has not been set'):
+        cont.to_dict()
+
+    # Test input validation
+    assert DummyContainer.validate_dict({'a': 3, 'b': 2.3}) is None
+
+    with pytest.raises(InputValidationError, match='There are missing options'):
+        DummyContainer.validate_dict({'a': 3})
+
+    with pytest.raises(ValueError, match='c is not'):
+        DummyContainer.validate_dict({'a': 3, 'b': 2.3, 'c': 2.0})
+
+    indict = {'a': 3, 'b': 2.3}
+    output = DummyContainer.serialise(indict)
+    assert output.get_dict() == indict
+
+    # Test catching invalid attribute
+    cont.c = 3.0
+    with pytest.raises(ValueError, match='not valid options'):
+        cont.to_dict()
+
+
+##### Existing tests
 
 
 class DummyOptions(OptionHolder):
